a13c46b4998f4c2cd627e71da840a147da3f2358
Jonathan <5377074j@gmail.com>
2021-08-20 14:43:46 +0300

    new filters to config the bot in a private chat

--- bot/filters.py
+++ bot/filters.py
@@ -1,5 +1,8 @@
 from pyrogram import filters
 
 group = filters.group
+private = filters.private
+start = filters.command("start")
+start_config = start & private & filters.create(lambda _, __, m: len(m.command) > 1)
 human = filters.regex("^human$")
 bot = filters.regex("^bot$")
